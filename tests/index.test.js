262cdacda0cc72b060513679686100bcfd9cdaf8
James Spencer <jameskcs1254@gmail.com>
2022-02-17 11:14:37 +0000

    Create index test file

---
+++ tests/index.test.js
@@ -0,0 +1,16 @@
+const index = require('../routes/index');
+
+const request = require('supertest');
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/', index);
+
+test('index route works', done => {
+  request(app)
+    .get('/')
+    .expect('Content-Type', /json/)
+    .expect({ message: 'test' })
+    .expect(200, done);
+});
