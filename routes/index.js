2d0d9724ff4f55bb538639d0cdbc694037216a46
James Spencer <jameskcs1254@gmail.com>
2022-02-17 11:25:16 +0000

    Update JSON response body.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ message: 'test' });
+  res.json({ message: 'This is the FakeBook API index page. Please connect to an endpoint with data.' });
 });
 
 module.exports = router;
