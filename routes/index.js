af3bbca4bf80e6df5d0aad9d1f7be3a84c9414c7
James Spencer <jameskcs1254@gmail.com>
2022-02-25 14:48:25 +0000

    Update to es6 syntax

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from "express";
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ message: 'This is the FakeBook API index page. Please connect to an endpoint with data.' });
 });
 
-module.exports = router;
+export default router;
