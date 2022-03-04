d262a758a4533c26794f6d28ac0a80a9f38587c4
James Spencer <jameskcs1254@gmail.com>
2022-03-04 12:17:35 +0000

    Remove comment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ message: 'This is the FakeBook API index page. Please connect to an endpoint with data.' });
 });
