5e9576687a83fa6f91b8a491c07aa34384f1885b
James Spencer <jameskcs1254@gmail.com>
2022-03-07 14:10:59 +0000

    setup test file. Create /user GET test.

---
+++ tests/user.test.js
@@ -0,0 +1,27 @@
+import user from '../routes/user.js';
+
+import request from 'supertest';
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/', user);
+
+describe('getAllUsers', () => {
+  test('/users GET route returns array containing user objects with correct fields ', done => {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect((res)=> {
+        expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining({
+          firstName: expect.any(String),
+          lastName: expect.any(String),
+          email: expect.any(String),
+          friends: expect.arrayContaining([expect.any(Object)]),
+          friendRequests: expect.arrayContaining([expect.any(Object)])
+        })]));
+      }).expect(200, done);
+  });
+});
+
