02340e98f173ffdc0e6ad8405dc40f07cf4324f5
James Spencer <jameskcs1254@gmail.com>
2022-03-07 15:30:16 +0000

    import getAllUsers controller function

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { getAllUsers } from "../controllers/userController.js";
+
 const router = express.Router();
 
-router.get('/', controller);
+router.get('/', getAllUsers);
 
 export default router;
