5b1e09337565b44e69e05e802b5bb5d56f0f5e17
James Spencer <jameskcs1254@gmail.com>
2022-04-01 12:42:34 +0100

    Create simple tests for getAllUsers.

--- tests/user.test.js
+++ tests/user.test.js
@@ -8,20 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use('/', user);
 
-describe('getAllUsers', () => {
-  test('/users GET route returns array containing user objects with correct fields ', done => {
+describe('getAllUsers tests', () => {
+  test('if /users GET route returns 200 status code', done => {
     request(app)
       .get('/')
-      .expect('Content-Type', /json/)
-      .expect((res)=> {
-        expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining({
-          firstName: expect.any(String),
-          lastName: expect.any(String),
-          email: expect.any(String),
-          friends: expect.arrayContaining([expect.any(String)]), 
-          friendRequests: expect.arrayContaining([expect.any(String)])
-        })]));
-      }).expect(200, done);
+      .expect(200, done);
   });
+  test('if /users GET route returns correct content type', done => {
+    request(app)
+      .get('/')
+      .expect('content-type', 'application/json; charset=utf-8', done);
+  });
+
+  // Halted testing at this point as simple query to DB does not need testing.
 });
 
